c4506066272a4e5c0b247f028373b36029c251ef
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-04-24 13:57:38 -0500

    3

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
 
 # TODO: Implementar CLI según README.md
+def read_todo_file(file_path):
+    """Reads tasks from a file. Returns a list of tasks."""
+    try:
+        with open(file_path, 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"File {file_path} not found! Returning an empty to-do list.")
+        return []
+
+
+def write_todo_file(file_path, tasks):
+    """Writes tasks to a file."""
+    with open(file_path, 'w') as file:
+        for task in tasks:
+            file.write(task + '\n')
