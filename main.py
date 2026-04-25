58e1b5a30f21fd2e952df90b7be468f7e095facf
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-04-25 11:18:31 -0500

    7

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ try:
 
     if sys.argv[1] == "--help":
         print("""Usage: python main.py <file_path> <command> [arguments]...
-
 Commands:
   add "task"    - Add a task to the list.
   remove "task" - Remove a task from the list.
   view          - Display all tasks.
-
 Examples:
   python main.py tasks.txt add "Buy groceries"
   python main.py tasks.txt remove "Do laundry"
